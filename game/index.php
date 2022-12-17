f0ebb8c3c91286c24ad2de3f1e78c6916e521885
Lexsi2002 <68783407+Lexsi2002@users.noreply.github.com>
2022-12-17 11:02:03 +0300

    Add files via upload

---
+++ game/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="robots" content="noindex, nofollow" />
+<style type="text/css" media="screen">
+html, body { height:100%; background-color: #000000;}
+body { margin:0; padding:0; overflow:hidden; }
+iframe { border:#000000 solid 0px;}
+#flashContent { width:100%; height:100%; }
+</style>
+</head>
+<body>
+<div id="flashContent"><iframe src="/game/59/index.php?width=500&height=665" width='500' height='665' scrolling='no'></iframe><script>
+    window.RufflePlayer = window.RufflePlayer || {};
+    window.addEventListener("load", (event) => {
+        const ruffle = window.RufflePlayer.newest();
+        const player = ruffle.createPlayer();
+        const container = document.getElementById("container");
+        container.appendChild(player);
+        player.load("/game/59/quantum-entanglement.swf");
+    });
+</script>
+<script src="/ruffle/2022-09-19/ruffle.js"></script>
+</div></body>
+</html>
