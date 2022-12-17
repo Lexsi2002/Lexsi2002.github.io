6eda83cf6e821c067292aa91a0f72963f346662f
Lexsi2002 <68783407+Lexsi2002@users.noreply.github.com>
2022-12-17 11:01:00 +0300

    Add files via upload

---
+++ game/59/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<style type="text/css" media="screen">
+		html, body { height:100%; background-color: #000000;}
+		body { margin:0; padding:0; overflow:hidden; }
+		#flashContent { width:100%; height:100%; }
+		</style>
+	</head>
+	<body>
+		<div id="flashContent">
+			<object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" width="500" height="665" align="middle">
+				<param name="movie" value="quantum-entanglement.swf" />
+				<param name="quality" value="high" />
+				<param name="play" value="true" />
+				<param name="loop" value="true" />
+				<param name="wmode" value="window" />
+				<param name="scale" value="showall" />
+				<param name="allowFullScreen" value="true" />
+				<param name="menu" value="true" />
+				<param name="devicefont" value="false" />
+				<param name="salign" value="" />
+				<param name="allowScriptAccess" value="always" />
+				<!--[if !IE]>-->
+				<object type="application/x-shockwave-flash" data="quantum-entanglement.swf" width="500" height="665">
+					<param name="movie" value="quantum-entanglement.swf" />
+					<param name="quality" value="high" />
+					<param name="play" value="true" />
+					<param name="loop" value="true" />
+					<param name="wmode" value="window" />
+					<param name="scale" value="showall" />
+					<param name="menu" value="true" />
+					<param name="allowFullScreen" value="true" />
+					<param name="devicefont" value="false" />
+					<param name="salign" value="" />
+					<param name="allowScriptAccess" value="always" />
+				</object>
+				<!--<![endif]-->
+			</object>
+		</div>
+	</body>
+</html>
